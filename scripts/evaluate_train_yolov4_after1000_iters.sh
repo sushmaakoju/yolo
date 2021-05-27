3c839be35f57ab040cd21c8621b163d89459cb20
Sushma Akoju <sushma.ananda13@gmail.com>
2021-05-27 16:09:32 -0400

    add training scripts each for first 1000 iterations and after

---
+++ scripts/evaluate_train_yolov4_after1000_iters.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+echo "This script is for Yolo training for custom object detection."
+echo "This script assumes you modified the detector.c file in darknet/src folder to save weights every 1000 iterations."
+echo "This script assumes you have updated max_batches=rounded total number of training images and learning rate = 0.0001 / 4 (since we use 4 GPUS)."
+echo "This script assumes you are using 4 GPUs using weights generated from first 600 iterations yolov4_600.weights."
+echo "We use seperate folders for each of cross validation folds' weights so the saved weights files are not overwritten accidentally."
+
+read -n 1 -s -r -p "If above assumptions are met, press any key to continue or press CTRL+C to cancel this script."
+
+echo "The present working directory is `pwd`"
+DIR="$(dirname "${BASH_SOURCE[0]}")"  # Get the directory name
+DIR="$(realpath "${DIR}")" 
+echo "The present working directory is ${DIR}"
+
+if [ -d ${DIR} ] 
+then
+    echo "Directory ${DIR} exists." 
+else
+    echo "Error: Directory ${DIR} does not exists."
+    exit 9999 #exit with 9999 error code
+fi
+
+echo "This script assumes you have provided path to obj folder with images in your obj.data file."
+echo "Starting training for first 1000 iterations for train fold 3"
+./darknet detector train cross_validation_folds/training/cfg/obj3.data cross_validation_folds/training/cfg/yolov4-custom_cv3.cfg yolov4_600.weights -dont_show -mjpeg_port 8090 -map | tee log_cv_after1000_fold3.txt
+
+echo "Starting training for first 1000 iterations for train fold 4"
+./darknet detector train cross_validation_folds/training/cfg/obj4.data cross_validation_folds/training/cfg/yolov4-custom_cv4.cfg yolov4_600.weights -dont_show -mjpeg_port 8090 -map | tee log_cv_after1000_fold4.txt
+
+echo "Starting training for first 1000 iterations for train fold 5"
+./darknet detector train cross_validation_folds/training/cfg/obj5.data cross_validation_folds/training/cfg/yolov4-custom_cv5.cfg yolov4_600.weights -dont_show -mjpeg_port 8090 -map | tee log_cv_after1000_fold5.txt
+
+echo "Starting training for first 1000 iterations for train fold 6"
+./darknet detector train cross_validation_folds/training/cfg/obj6.data cross_validation_folds/training/cfg/yolov4-custom_cv6.cfg yolov4_600.weights -dont_show -mjpeg_port 8090 -map | tee log_cv_after1000_fold6.txt
+
+echo "Starting training for first 1000 iterations for train fold 7"
+./darknet detector train cross_validation_folds/training/cfg/obj7.data cross_validation_folds/training/cfg/yolov4-custom_cv7.cfg yolov4_600.weights -dont_show -mjpeg_port 8090 -map | tee log_cv_after1000_fold7.txt
+
+echo "Starting training for first 1000 iterations for train fold 8"
+./darknet detector train cross_validation_folds/training/cfg/obj8.data cross_validation_folds/training/cfg/yolov4-custom_cv8.cfg yolov4_600.weights -dont_show -mjpeg_port 8090 -map | tee log_cv_after1000_fold8.txt
+
+echo "Starting training for first 1000 iterations for train fold 9"
+./darknet detector train cross_validation_folds/training/cfg/obj9.data cross_validation_folds/training/cfg/yolov4-custom_cv9.cfg yolov4_600.weights -dont_show -mjpeg_port 8090 -map | tee log_cv_after1000_fold9.txt
+
+
+echo "Training first 1000 iterations is completed."
